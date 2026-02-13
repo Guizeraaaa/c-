a13e266f1d99df63c02846f68c7ba28bb206ad6b
guilherme <guilhermebatistacorreia@gmail.com>
2026-02-13 11:27:47 -0300

    continuando com a aula

---
+++ Variaves.cpp
@@ -0,0 +1,37 @@
+ #include <iostream>
+
+ using namespace std; 
+
+ int main(){
+    int inteiro;
+    inteiro = 5;
+    cout << inteiro << endl;
+
+    float real;
+    real = 5.2;
+    cout << real << endl;
+
+    double real2;
+    real2 = 5.2e99;
+    cout << real2 <<endl;
+
+    bool booleano;
+    booleano = true;
+    cout << booleano << endl;
+
+    char caractere;
+    caractere = 'a';
+    cout << caractere << endl;
+
+    string letras;
+    letras = "abc";
+    cout << letras << endl;
+
+    int idade;
+    cout << "qual a sua idade \n";
+    cin >> idade;
+    cout << "Idade " << idade << endl;
+    
+    
+    return 0;
+ }
