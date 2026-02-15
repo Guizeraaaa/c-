d544a62cb22a4da2d90c14bdbbd3aa6803ebfb6d
guilherme <guilhermebatistacorreia@gmail.com>
2026-02-15 18:25:18 -0300

    ending four

---
+++ challenge_three.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+void mensagem(){
+    cout << "Digite um numero inteiro" << endl;
+}
+
+
+int main(){
+    float n, fatorial =1;
+
+    mensagem();
+    cin >> n;
+
+    for ( int i = 1; i <= n; i++)
+    {
+fatorial *= i;   
+ }
+    
+cout << "O resultado é de: " << fatorial << endl;
+
+    return 0;
+}
