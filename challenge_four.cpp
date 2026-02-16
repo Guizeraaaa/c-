3f5cad519c7535c56c78dc5f077cf6c6e97aba4b
guilherme <guilhermebatistacorreia@gmail.com>
2026-02-16 10:30:30 -0300

    terminando ex 4

--- challenge_four.cpp
+++ challenge_four.cpp
@@ -1,2 +1,28 @@
 #include<iostream>
 using namespace std; 
+
+void mensagemBase() {
+    cout <<  "Digite a base" << endl;
+}
+
+void mensagempotencia() {
+    cout <<  "Digite a potencia" << endl;
+}
+
+int main(){
+    
+    float base, potencia, n, resultado;
+    mensagemBase();
+    cin >> base;
+
+    mensagempotencia();
+    cin >> potencia;
+
+    for (int i=1; i <= potencia; i++ ){
+             resultado = base * base;
+             n = resultado * base;
+    }
+    
+    cout << "o resultado e de: " << n << endl;
+    return 0;
+}
